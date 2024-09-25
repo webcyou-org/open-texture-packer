840eab3d7d6e54d7a40e7b5740437756e0e2cc77
panicdragon <d.takayama.web@gmail.com>
2024-09-26 00:27:05 +0900

    feat: 🎸 add clap

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,31 @@ mod constant;
 
 use std::fs;
 use std::path::Path;
-use std::env;
 use file_io::collect_image_paths;
 use sprite_packer::generate_texture_sheets;
 use texture_sheet::calculate_sheet_dimensions;
 use constant::{MAX_SHEET_WIDTH, MAX_SHEET_HEIGHT, DEFAULT_INPUT_DIR, DEFAULT_OUTPUT_DIR};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    #[clap(default_value_t = String::from(DEFAULT_INPUT_DIR))]
+    input_dir: String,
+    #[clap(default_value_t = String::from(DEFAULT_OUTPUT_DIR))]
+    output_dir: String,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
+    let args = Cli::parse();
 
-    let input_dir = if args.len() < 2 {
-        String::from(DEFAULT_INPUT_DIR)
-    } else {
-        args[1].clone()
-    };
-
-    let output_dir = if args.len() >= 3 {
-        Path::new(&args[2])
-    } else {
-        Path::new(DEFAULT_OUTPUT_DIR)
-    };
+    let input_dir = &args.input_dir;
+    let output_dir = Path::new(&args.output_dir);
 
     if !output_dir.exists() {
         fs::create_dir_all(output_dir)?;
     }
 
-    let image_paths = collect_image_paths(input_dir)?;
+    let image_paths = collect_image_paths(input_dir.to_string())?;
     if image_paths.is_empty() {
         eprintln!("No image files found in the directory.");
         std::process::exit(1);
